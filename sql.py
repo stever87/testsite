ca779419a4ea0ae3bb6833d40e0e3435b6fcb661
stever87 <furi_d@hotmail.com>
2016-01-02 22:51:45 +0000

    added bootstrap style and sql.py

---
+++ sql.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+#use to empty shell for site.db
+
+with sqlite3.connect("site.db") as connection:
+	c = connection.cursor()
+	c.execute("""DROP TABLE posts""")
+	c.execute("""CREATE TABLE `posts` (
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`title`	TEXT NOT NULL,
+	`content`	TEXT NOT NULL)""")
+	
+
